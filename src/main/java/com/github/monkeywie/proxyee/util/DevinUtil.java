8820028e05003e5b2e273a550f6c20a8ede867be
chenqp <>
2024-09-27 17:13:45 +0800

    支持websocket暂时提交

--- src/main/java/com/github/monkeywie/proxyee/util/DevinUtil.java
+++ src/main/java/com/github/monkeywie/proxyee/util/DevinUtil.java
@@ -1,9 +1,11 @@
 package com.github.monkeywie.proxyee.util;
 
+import com.github.monkeywie.proxyee.crt.CertUtilsLoader;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
-import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
@@ -58,11 +60,11 @@ public class DevinUtil {
      * 关闭帧 (0x88): 表示关闭连接的请求。
      * Ping 帧 (0x89): 用于保持连接活跃。
      * Pong 帧 (0x8A): 对 Ping 帧的回应。
-     * @param requestBody
+     * @param msg
      * @return
      */
-    public static ByteBuf getWebsocketMst81(String requestBody) {
-        byte[] messageBytes = requestBody.getBytes(StandardCharsets.UTF_8);
+    public static ByteBuf getWebsocketMst81(String msg) {
+        byte[] messageBytes = msg.getBytes(StandardCharsets.UTF_8);
         int length = messageBytes.length;
 
         // 设置帧头，0x81 表示文本帧，length 的 16 进制表示
@@ -71,5 +73,20 @@ public class DevinUtil {
         // 合并前缀和内容
         ByteBuf fullMessage = Unpooled.wrappedBuffer(prefix, content);
         return fullMessage;
+    }
+
+    /**
+     * 自定义返回JSON响应
+     * @param msg
+     * @return
+     */
+    public static FullHttpResponse getFullHttpResponse(String msg) {
+        ByteBuf customContentBuf = Unpooled.copiedBuffer(msg, CharsetUtil.UTF_8);
+
+        // 创建完整的 HTTP 响应
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, customContentBuf);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json; charset=UTF-8");
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, customContentBuf.readableBytes());
+        return response;
     }
 }
